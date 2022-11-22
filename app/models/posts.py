5e71744197a9c6d6a0541f538a72df67461615a8
linder3hs <linderhassinger00@gmail.com>
2022-11-21 20:31:38 -0500

    feat: add api post

--- app/models/posts.py
+++ app/models/posts.py
@@ -7,3 +7,11 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "body": self.body,
+            "timestamp": self.timestamp,
+            "user_id": self.user_id,
+        }
--- app/routes/post.py
+++ app/routes/post.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from flask_login import current_user, login_required
 from app.db import db
 from app.models.posts import Post
@@ -32,3 +32,19 @@ def post_detail(id):
     context = {"comment_form": comment_form, "post": post}
 
     return render_template("post-detail.html", **context)
+
+
+@post_router.route("/api/post", methods=["POST"])
+def api_post():
+    # * Es la infomracion que nos envia el cliente
+    # * recuerden que con .json transforma la data y puedo leerle desde py
+    # * para este caso solo requiero 2 cosas body, user_id
+    # ! {"body": "Texto del post", "user_id": 4}
+    client_post_information = request.json
+    new_post = Post(
+        body=client_post_information["body"], user_id=client_post_information["user_id"]
+    )
+    db.session.add(new_post)
+    db.session.commit()
+    # Nota: El estado de una creacion es 201: created
+    return (jsonify(new_post.to_json()), 201)
